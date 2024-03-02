61c911db401e8afe68722577b40ab523ad2b1287
govindrevenger <govindalbert2014@gmail.com>
2024-03-02 09:43:14 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,16 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+]);
+
+console.log(heading); //object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
